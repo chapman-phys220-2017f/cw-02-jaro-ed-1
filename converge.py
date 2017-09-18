6d36a6fd667dfc53d368047b8f0a1ddb070539fd
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 01:45:06 -0700

    Instructor comments

--- converge.py
+++ converge.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
-import scipy
+import scipy                ### Not needed, so remove
 
 def compute_sum(tol=1e-2):
-	sum = 0;
+	sum = 0;                            ### No semicolons needed
 	k = 1;
 	diff = 1;
 	while diff >= tol:
-		sum_save = sum;
-		sum += 1/k**2;
-		diff = sum - sum_save;
+		sum_save = sum;             
+		sum += 1/k**2;               
+		diff = sum - sum_save;      ### How did you know this would be positive?
 		k += 1;
 	return sum
 
