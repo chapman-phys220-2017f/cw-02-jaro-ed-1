f3ca1136a9574385a8d4a19d6638eb7503c198ad
Justin Dressel <prof.justin.dressel@gmail.com>
2017-09-18 01:46:59 -0700

    Instructor comment

--- fibs.py
+++ fibs.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 def fibs(n):
-	f_list = [1,1];
+	f_list = [1,1];           ### no semicolons. DO NOT USE TABS. Use 4 spaces to indent.
 	a,b = 1,1;
 	for num in range(2,n):
 		a,b = b,a+b;
@@ -14,6 +14,12 @@ def fib_generator():
 		yield a
 		a,b = b, a+b;
 
+###
+# The following code should be in a __main__ block. Do not put executable code outside of a
+# main block. You want to import the module, and use the functions above directly. If you want
+# a convenience function that does the below actions, put it in a function and call that function
+# explicitly when needed.
+###
 
 fibs_list = fibs(5);
 print(fibs_list)
